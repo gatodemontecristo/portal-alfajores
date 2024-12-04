a28fa33fb20d82dd0135809548a2fa613dd5e9ba
gatodemontecristo <erickdevicente2024@outlook.com>
2024-12-04 02:52:42 -0500

    feat: implemented mock data

--- src/ui/components/TittleBar.tsx
+++ src/ui/components/TittleBar.tsx
@@ -1,8 +1,16 @@
 export const TittleBar = () => {
   return (
-    <nav className="w-screen bg-primary text-white flex flex-row justify-start items-center px-6 gap-6">
-      <img src="../logo/tinos.png" className="w-[80px] h-[80px]" alt="" />
-      <h2 className="text-white font-bold text-xl">Portal de los alfajores</h2>
+    <nav className="w-screen bg-primary text-white flex flex-row justify-between items-center px-7 gap-6">
+      <div className="flex flex-row  items-center">
+        <img src="../logo/tinos.png" className="w-[120px] h-[80px]" alt="" />
+        <h2 className="text-white font-bold text-xl ms-5">
+          Portal de los alfajores
+        </h2>
+      </div>
+      <div className="flex flex-row  items-center">
+        <h2 className="text-white font-bold text-xl ms-5">Spring Q4 SP5 | </h2>
+        <h4 className="text-white font-bold text-sm ms-5">27 Nov - 10 Dec</h4>
+      </div>
     </nav>
   );
 };
