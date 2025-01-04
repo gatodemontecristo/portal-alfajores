e51c6ab087dddcad19778e905a4888a9f97e4bd5
gatodemontecristo <erickdevicente2024@outlook.com>
2025-01-03 21:22:17 -0500

    feat: added new history section

--- src/statistics/pages/HistoryPage.tsx
+++ src/statistics/pages/HistoryPage.tsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
+import { Skeleton } from '../../ui';
+import { ToogleButton, UserAlert } from '../components';
 import Carousel from './Carousel';
+import { nanoid } from 'nanoid';
+import { AlfajorSpringProps } from '../../interfaces';
 
 export const HistoryPage = () => {
+  const [isLate, setIsLate] = useState(true);
+  const handleToggle = () => {
+    setIsLate(!isLate);
+  };
+  const [alfajorCollection, setAlfajorCollection] =
+    useState<AlfajorSpringProps | null>(null);
+
+  const handleOpenModal = () => {};
   return (
     <>
       <Carousel>
@@ -41,7 +54,51 @@ export const HistoryPage = () => {
             </p>
           </div>
         </div>
-        <div className="w-full md:w-1/3  p-3 h-full"></div>
+        <div className="w-full md:w-1/3  p-3 h-full">
+          <div className="flex flex-row items-center justify-evenly h-[10%] top-0  pt-[20px] w-full">
+            <ToogleButton
+              handleToggle={handleToggle}
+              isAction={isLate}
+              children1={<span>Pagaron</span>}
+              children2={<span>Concursaron</span>}
+            />
+            <div className="flex flex-col gap-1">
+              <div className="flex flex-rown gap-2 items-center">
+                <div className="w-[20px] h-[20px] bg-slate-400"></div>
+                <p className="text-xs">Tardanza</p>
+              </div>
+              <div className="flex flex-row gap-2 items-center">
+                <div className="w-[20px] h-[20px] bg-yellow-500"></div>
+                <p className="text-xs">Supertardanza</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row items-center w-full justify-center  flex-wrap  gap-4 overflow-y-scroll custom-scrollbar">
+            {alfajorCollection === null
+              ? Array.from({ length: 6 }).map(() => (
+                  <div
+                    className="flex flex-col items-center w-1/4 gap-2 mt-[15%]"
+                    key={nanoid()}
+                  >
+                    <Skeleton type="circle" extraClass="w-[10vh] h-[10vh]" />
+                    <Skeleton type="rect" extraClass="w-full h-[25px]" />
+                  </div>
+                ))
+              : alfajorCollection?.users
+                  .filter((user) =>
+                    isLate
+                      ? user.tardanzas.length > 0
+                      : user.tardanzas.length == 0,
+                  )
+                  .map((user) => (
+                    <UserAlert {...{ user, handleOpenModal }} key={nanoid()}>
+                      <UserAlert.Image />
+                      <UserAlert.Info />
+                      <UserAlert.Date />
+                    </UserAlert>
+                  ))}
+          </div>
+        </div>
       </Carousel>
     </>
   );
