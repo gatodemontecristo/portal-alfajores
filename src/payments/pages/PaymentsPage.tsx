8e147f132357d5c1f5dfa1e33af7904d06177437
gatodemontecristo <erickdevicente2024@outlook.com>
2025-04-22 00:42:48 -0500

    feat: Last fix

--- src/payments/pages/PaymentsPage.tsx
+++ src/payments/pages/PaymentsPage.tsx
@@ -3,7 +3,7 @@ import Carousel from '../../statistics/pages/Carousel';
 export const PaymentsPage = () => {
   return (
     <>
-      <Carousel>
+      <Carousel page="PaymentsPage">
         <div className="w-full md:w-1/3 relative h-full"></div>
 
         <div className="w-full md:w-1/3 flex flex-col relative items-center justify-start gap-5"></div>
